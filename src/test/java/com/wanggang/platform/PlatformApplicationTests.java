869b968c73b115942c7986355b835c347fce48ea
untilYou <wanggang@until-you-possibles-MacBook-Pro.local>
2021-08-20 00:33:31 +0800

    初始化项目

---
+++ src/test/java/com/wanggang/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanggang.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
