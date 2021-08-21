1f986b8405101ee04ebf895b8d2af5b130eefd90
wanggang <arthurwanggang@yeah.net>
2021-08-21 11:46:19 +0800

    1: 连接阿里云RDS，新建对应的表结构 2: mybatis-generator逆向生成 interface xml 3: 修改jdbc驱动版本 4: 测试使用idea自带的httpClient,不咋的...

---
+++ src/main/java/com/wanggang/platform/mapper/EbookSnapshotMapper.java
@@ -0,0 +1,30 @@
+package com.wanggang.platform.mapper;
+
+import com.wanggang.platform.domain.EbookSnapshot;
+import com.wanggang.platform.domain.EbookSnapshotExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookSnapshotMapper {
+    long countByExample(EbookSnapshotExample example);
+
+    int deleteByExample(EbookSnapshotExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(EbookSnapshot record);
+
+    int insertSelective(EbookSnapshot record);
+
+    List<EbookSnapshot> selectByExample(EbookSnapshotExample example);
+
+    EbookSnapshot selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByExample(@Param("record") EbookSnapshot record, @Param("example") EbookSnapshotExample example);
+
+    int updateByPrimaryKeySelective(EbookSnapshot record);
+
+    int updateByPrimaryKey(EbookSnapshot record);
+}
